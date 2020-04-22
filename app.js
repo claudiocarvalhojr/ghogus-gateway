fe4ce3ba7ea1729f21dc6234b906470dcdf76abc
claudiocarvalhojr <claudio.omar.jr@gmail.com>
2020-04-22 19:58:27 -0300

    alterações diversas

--- app.js
+++ app.js
@@ -1,125 +1,163 @@
-//index.js
 require('dotenv-safe').config()
-var http = require('http')
+const http = require('http')
 const express = require('express')
-const httpProxy = require('express-http-proxy')
-const app = express()
-var cookieParser = require('cookie-parser')
+const expressHttpProxy = require('express-http-proxy')
+const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
-var jwt = require('jsonwebtoken')
-var logger = require('morgan')
+const jwt = require('jsonwebtoken')
+const logger = require('morgan')
 const helmet = require('helmet')
 const cors = require('cors')
 
-const userServiceProxy = httpProxy('http://localhost:3001')
+var dbConfig = require('./db');
+var mongoose = require('mongoose');
+var useUnifiedTopology = { useUnifiedTopology: true, useNewUrlParser: true }
+mongoose.connect(dbConfig.url, useUnifiedTopology);
+var User = require('./models/user');
+
+const app = express()
+
+// var customersServiceProxy = httpProxy('http://localhost:3001')
+// var usersServiceProxy = httpProxy('http://localhost:3001')
+// var productsServiceProxy = httpProxy('http://localhost:3001')
+
+var apiServiceProxy = expressHttpProxy('http://localhost:3001')
 
 const port = process.env.PORT || 3000
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    res.header('Access-Control-Allow-Credentials', '*')
-    res.header('Access-Control-Expose-Headers', 'x-access-token')
-    next()
-})
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Headers', '*')
+//     res.header('Access-Control-Allow-Credentials', '*')
+//     res.header('Access-Control-Expose-Headers', 'Authorization')
+//     next()
+// })
 
 app.use(logger('dev'))
 app.use(helmet())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
-// app.use(cors());
+
 const corsOptions = {
     exposedHeaders: 'Authorization'
 }
-  
+
 app.use(cors(corsOptions));
 
-//rota protegida
-app.get('/clientes', verifyJWT, (req, res, next) => {
-    console.log('server/clientes...')
-    // console.log('Retornou todos clientes!')
-    // res.status(200).json([{id:1, nome:'claudio'}])
-    userServiceProxy(req, res, next)
+function log(message) {
+    var data = new Date()
+    console.log('****************************************')
+    console.log(data.toLocaleDateString() + ' ' + data.toLocaleTimeString() + ' - ' + message)
+    // console.log('****************************************')
+}
+
+app.get('/user', verifyJWT, (req, res, next) => {
+//    let id = req.query.id
+//   console.log('\nID: ' + id + '\n')
+    log('server/user...')
+    apiServiceProxy(req, res, next)
 })
 
-//rota de login
+app.get('/customers', verifyJWT, (req, res, next) => {
+    log('server/customers...')
+    apiServiceProxy(req, res, next)
+})
+
+app.get('/users', verifyJWT, (req, res, next) => {
+    log('server/users...')
+    apiServiceProxy(req, res, next)
+})
+
+app.get('/products', (req, res, next) => {
+    log('server/products...')
+    apiServiceProxy(req, res, next)
+})
+
+app.get('/product/:id', (req, res, next) => {
+    log('server/product...')
+    apiServiceProxy(req, res, next)
+})
+
+app.post('/products', verifyJWT, (req, res, next) => {
+    log('server/products...')
+    apiServiceProxy(req, res, next)
+})
+
+app.get('/images', verifyJWT, (req, res, next) => {
+    log('server/images...')
+    apiServiceProxy(req, res, next)
+})
+
+app.post('/images', verifyJWT, (req, res, next) => {
+    log('server/post images...')
+    apiServiceProxy(req, res, next)
+})
+
+app.get('/images/last', verifyJWT, (req, res, next) => {
+    log('server/get last images...')
+    apiServiceProxy(req, res, next)
+})
+
 app.post('/login', (req, res, next) => {
-
-    console.log('server/login...')
-    // console.log('user: ', req.body.user)
-    // console.log('pass: ', req.body.pass)
-
-    let user = req.body.user
-    let pass = req.body.pass
-
-    if (user === 'claudio' && pass === '123') {
-        const id = 1
-        var token = jwt.sign({ id }, process.env.SECRET, { expiresIn: 30 })
-        console.log('Fez login e gerou token!')
-        return res.status(200).json({ 
-            user: req.body.user, 
-            auth: true, 
-            message: 'Login OK',
-            token: token
+    log('server/login...')
+    User.findOne({ _id: req.body.id, email: req.body.email }, (err, doc) => {
+        if (err)
+            return res.status(500).json({
+                auth: false,
+                message: 'Falha ao fazer login!',
+                token: null
+            })
+        if (doc != null) {
+            let id = doc._id
+            let token = jwt.sign({ id }, process.env.TOKEN_SECRET, { expiresIn: 30 * 60 })
+            return res.status(200).json({
+                auth: true,
+                message: 'Login OK!',
+                token: token
+            })
+        }
+        return res.status(500).json({
+            auth: false,
+            message: 'Login inválido!',
+            token: null
         })
-    }
-    console.log('Login falhou!')
-    return res.status(500).json({ 
-        auth: false, 
-        message: 'Login inválido!',
-        token: null
     })
 })
 
-//rota de logout
-app.post('/logout', function (req, res) {
-    console.log('Fez logout e cancelou o token!')
-    res.status(200).json({ 
-        auth: false, 
+app.get('/logout', function (req, res) {
+    log('server/logout...')
+    res.status(200).json({
+        auth: false,
         message: 'Logout OK',
-        token: null 
+        token: null
     })
 })
 
 app.get('/check', verifyJWT, (req, res, next) => {
-    console.log('server/check...')
+    log('server/check...')
     res.status(200).json({
-        auth: true, 
+        auth: true,
         message: 'Check OK'
     })
 })
 
-//função que verifica se o JWT é ok
 function verifyJWT(req, res, next) {
-
-    console.log('\nserver/verifyJWT...')
-
-    // console.log('Headers 1: ' + JSON.stringify(res.getHeadernames()))
-    // console.log('Headers 2: ' + JSON.stringify(res.getHeaders()))
-
-    // console.log(JSON.stringify(req.headers));
-
-    // let token = req.query.token || req.headers['x-access-token']
-    let token = req.body.token || req.query.token || req.headers['x-access-token']
-    
-    // console.log('x-access-token: ' + res.hasHeader('x-access-token'))
-
-    // console.log('Token: ' + token)
-
-    if (!token) return res.status(401).json({ 
-        auth: false, 
-        message: 'Token não informado.',
+    log('server/verifyJWT...')
+    let token = req.query.token || req.body.token || req.headers['x-access-token']
+    if (!token) return res.status(401).json({
+        auth: false,
+        message: 'Token não informado!',
         token: null
     })
-    jwt.verify(token, process.env.SECRET, function (err, decoded) {
-        if (err) return res.status(500).json({ 
-            auth: false, 
-            message: 'Token inválido.',
-            token: null
-        })
-        // req.userId = decoded.id
-        // console.log('User Id: ' + decoded.id)
+    jwt.verify(token, process.env.TOKEN_SECRET, function (err, decoded) {
+        if (err)
+            return res.status(500).json({
+                auth: false,
+                message: 'Token inválido!',
+                token: null
+            })
+        // console.log('DECODED ID: ' + decoded.id)
         next()
     })
 }
